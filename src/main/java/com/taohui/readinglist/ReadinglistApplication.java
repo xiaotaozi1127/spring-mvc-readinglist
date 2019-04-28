4bc51f25ea289e6d61efdd850428e9bce03eb99f
taohui <htao@thoughtworks.com>
2019-04-28 10:26:52 +0800

    [taohui] initialize a spring web application via spring initializr

---
+++ src/main/java/com/taohui/readinglist/ReadinglistApplication.java
@@ -0,0 +1,13 @@
+package com.taohui.readinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadinglistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadinglistApplication.class, args);
+    }
+
+}
