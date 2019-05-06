b327a3fe3d2606322cc5a5eb5e39670c7dfd6ff0
taohui <htao@thoughtworks.com>
2019-05-06 12:31:40 +0800

    [taohui] override default security config and error page

---
+++ src/main/java/com/taohui/readinglist/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.taohui.readinglist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private ReaderRepository readerRepository;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/").access("hasRole('READER')")
+                .antMatchers("/**").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .failureUrl("/login?error=true");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(username -> {
+            Optional<Reader> userDetails = readerRepository.findById(username);
+            if (userDetails != null) {
+                return userDetails.get();
+            }
+            throw new UsernameNotFoundException("User '" + username + "' not found.");
+        });
+    }
+}
