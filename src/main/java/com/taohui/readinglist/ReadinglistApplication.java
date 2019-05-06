e9763be98d8fd9af4cfb02f127e601a9ec1ac9f1
taohui <htao@thoughtworks.com>
2019-05-06 13:04:39 +0800

    [taohui] set initial data via import.sql

--- src/main/java/com/taohui/readinglist/ReadinglistApplication.java
+++ src/main/java/com/taohui/readinglist/ReadinglistApplication.java
@@ -2,12 +2,19 @@ package com.taohui.readinglist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class ReadinglistApplication {
+public class ReadinglistApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(ReadinglistApplication.class, args);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
     }
 
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into reader (username, password, fullname) values ('hui', 'password', 'Craig Walls');
+insert into reader (username, password, fullname) values ('tao', 'password', 'Walt Disney');
