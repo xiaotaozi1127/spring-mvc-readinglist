54e31f602ae181f938202af492e59176d084261b
taohui <htao@thoughtworks.com>
2019-05-07 18:47:50 +0800

    [taohui] use in memory login credentials

---
+++ src/main/java/com/taohui/readinglist/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.taohui.readinglist;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("root")
+                .password("root")
+                .roles("USER")
+                .and()
+                .passwordEncoder(new CustomPasswordEncoder());
+    }
+}
