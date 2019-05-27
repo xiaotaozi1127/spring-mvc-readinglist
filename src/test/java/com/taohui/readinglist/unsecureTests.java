8a6d35e2607a92ccb2ce503c2d258dc31dd35293
taohui <htao@thoughtworks.com>
2019-05-27 15:56:27 +0800

    [taohui] after post new book, test get books should contain posted book

--- src/test/java/com/taohui/readinglist/unsecureTests.java
+++ src/test/java/com/taohui/readinglist/unsecureTests.java
@@ -10,6 +10,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -42,16 +44,32 @@ public class unsecureTests {
 
     @Test
     public void postBook() throws Exception {
+        String book_title = "BOOK TITLE";
+        String book_author = "BOOK AUTHOR";
+        String book_isbn = "1234567890";
+        String book_description = "DESCRIPTION";
         mockMvc.perform(post("/readingList/th")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .param("title", "BOOK TITLE")
-                .param("author", "BOOK AUTHOR")
-                .param("isbn", "1234567890")
-                .param("description", "DESCRIPTION"))
+                .param("title", book_title)
+                .param("author", book_author)
+                .param("isbn", book_isbn)
+                .param("description", book_description))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(header().string("Location", "/readingList/th"));
 
+        Book expectedBook = new Book();
+        expectedBook.setTitle(book_title);
+        expectedBook.setAuthor(book_author);
+        expectedBook.setDescription(book_description);
+        expectedBook.setIsbn(book_isbn);
+        expectedBook.setReader("th");
+        expectedBook.setId(1L);
 
+        mockMvc.perform(get("/readingList/th"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("readingList"))
+                .andExpect(model().attributeExists("books"))
+                .andExpect(model().attribute("books", hasSize(1)))
+                .andExpect(model().attribute("books", contains(samePropertyValuesAs(expectedBook))));
     }
-
 }
