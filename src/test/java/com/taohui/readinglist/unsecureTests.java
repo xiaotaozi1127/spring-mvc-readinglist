69b2d27c243037524329e79ffcf24d1882e2dbc5
taohui <htao@thoughtworks.com>
2019-05-27 15:42:37 +0800

    [taohui] test post new book result

--- src/test/java/com/taohui/readinglist/unsecureTests.java
+++ src/test/java/com/taohui/readinglist/unsecureTests.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -37,6 +38,20 @@ public class unsecureTests {
                 .andExpect(view().name("readingList"))
                 .andExpect(model().attributeExists("books"))
                 .andExpect(model().attribute("books", Matchers.is(Matchers.empty())));
+    }
+
+    @Test
+    public void postBook() throws Exception {
+        mockMvc.perform(post("/readingList/th")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("title", "BOOK TITLE")
+                .param("author", "BOOK AUTHOR")
+                .param("isbn", "1234567890")
+                .param("description", "DESCRIPTION"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/readingList/th"));
+
+
     }
 
 }
