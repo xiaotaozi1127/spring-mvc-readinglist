da51a2030da1e9b73a2627dec0c1422e02eb254e
taohui <htao@thoughtworks.com>
2019-05-30 19:20:51 +0800

    [taohui] test authenticated user with mocked user

--- src/test/java/com/taohui/readinglist/secureTest.java
+++ src/test/java/com/taohui/readinglist/secureTest.java
@@ -5,12 +5,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -35,5 +39,22 @@ public class secureTest {
         mockMvc.perform(get("/readingList/th"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(header().string("Location", "http://localhost/login"));
+    }
+
+    @Test
+//    @WithUserDetails("hui")
+    @WithMockUser(username="hui")
+    public void homePage_authenticatedUser() throws Exception {
+        Reader expectedReader = new Reader();
+        expectedReader.setUsername("hui");
+        expectedReader.setPassword("password");
+        expectedReader.setFullname("taohui");
+
+        mockMvc.perform(get("/readingList/th"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("readingList"))
+//                .andExpect(model().attribute("reader",
+//                        samePropertyValuesAs(expectedReader)))
+                .andExpect(model().attribute("books", hasSize(0)));
     }
 }
