8c202f806c33a292f20318de43bc5faf5ec1b5bf
taohui <htao@thoughtworks.com>
2019-07-25 15:27:12 +0800

    [taohui] set in memory user for login

--- src/main/java/com/taohui/readinglist/WebSecurityConfig.java
+++ src/main/java/com/taohui/readinglist/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.taohui.readinglist;
 
+//import com.taohui.readinglist.Entity.Reader;
+//import com.taohui.readinglist.Repository.ReaderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,33 +17,46 @@ import java.util.Optional;
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private ReaderRepository readerRepository;
+//    @Autowired
+//    private ReaderRepository readerRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
+//        http
+//                .authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .formLogin()
+//                .and()
+//                .httpBasic();
+////                .and().csrf().disable();
+
+        http.authorizeRequests()
+                .antMatchers("/readingList/*").access("hasRole('READER')")
+                .antMatchers("/**").permitAll()
                 .and()
-                .formLogin()
-                .and().csrf().disable();
+                .formLogin().and().httpBasic();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(new UserDetailsService() {
-            @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                Optional<Reader> reader = readerRepository.findById(username);
-                if(reader.get() != null)
-                {
-                    return reader.get();
-                }
-                throw new UsernameNotFoundException("User '" + username + "' not found.");
-            }
-        })
+//        auth.userDetailsService(new UserDetailsService() {
+//            @Override
+//            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//                Optional<Reader> reader = readerRepository.findById(username);
+//                if(reader.get() != null)
+//                {
+//                    return reader.get();
+//                }
+//                throw new UsernameNotFoundException("User '" + username + "' not found.");
+//            }
+//        })
+//        .passwordEncoder(new CustomPasswordEncoder());
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("READER")
+        .and()
         .passwordEncoder(new CustomPasswordEncoder());
     }
 }
